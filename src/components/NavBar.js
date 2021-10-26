b5f57956c21412da90e591bba6b9edea19c5cf45
pMrtnz <patomartinez010@gmail.com>
2021-10-26 12:21:17 -0300

    Adding navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../assets/img/logo.svg';
+import cartLogo from '../assets/img/cartLogo.png';
+
+function NavBar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light ">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src={logo} alt="logo" className="img-fluid mainLogo"/>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse fs-4" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ms-auto">
+                        <a className="nav-link active px-5" aria-current="page" href="#">Home</a>
+                        <a className="nav-link px-5" href="#">Shop</a>
+                        <a className="nav-link px-5" href="#">About</a>
+                        <a className="nav-link py-0 mx-4" href="#">
+                            <img src={cartLogo} alt="Cart" className="cartIcon"/>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+  );
+}
+
+export default NavBar;
